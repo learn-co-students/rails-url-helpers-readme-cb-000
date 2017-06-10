e7f5925c0dc4294ebecc41c52d802e709655d0a2
Mohsen Danesh Pajouh <mohsendp@gmail.com>
2017-06-10 17:10:36 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+# We can have multiple path referring to the same action in a controller for example we can say
+# get 'salam' => 'posts#index'
+# get 'posts' => 'posts#index'
+# get 'chetori' => 'posts#index'
+# get '/listpost', to: 'posts#index', as: 'listpost'
+# The benefit of having 'as:' is that we can use _path or _url with our desired custom name
+
+  # get 'posts' => 'posts#index', as: :stsop
   resources :posts, only: [:index, :show]
 end
